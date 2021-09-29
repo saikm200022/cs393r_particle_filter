511e2913c805fa7414d5663cf03df962a6e69554
Shreyas Amargol <amargol@cs.utexas.edu>
2021-09-28 19:51:55 -0500

    clear particles initialize

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /u/amargol/Masters/Robots/projects/cs393r_starter/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
